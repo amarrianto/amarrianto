f274683d1bc7e34da496411df6c2706694e245a1
amarrianto <amarrianto6969@gmail.com>
2021-02-21 21:32:01 +0700

    first commit

---
+++ models/orderDetail.model.js
@@ -0,0 +1,40 @@
+const orderDetail = (sequelize, DataTypes)=>{
+    const OrderDetail = sequelize.define('order_detail', {
+        ordi_quantity: {
+          type: DataTypes.INTEGER,
+          allowNull: true
+        },
+        ordi_price: {
+          type: DataTypes.INTEGER,
+          allowNull: true
+        },
+        ordi_cart_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        ordi_prod_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        ordi_order_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          references: {
+            model: 'orders',
+            key: 'order_name'
+          }
+        }
+      }, {
+        sequelize,
+        tableName: 'order_detail',
+        schema: 'public',
+        timestamps: false
+      });
+      OrderDetail.removeAttribute('id');
+      OrderDetail.associate = models => {
+      OrderDetail.belongsTo(models.orders, {onDelete: 'CASCADE' });
+      OrderDetail.belongsTo(models.cart,{onDelete: 'CASCADE' })
+      }
+      return OrderDetail;
+    }
+export default orderDetail;
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import userController from '../controllers/users.controller'
+import apiUserOrder from '../controllers/apiUserOrder'
+const router = Router()
+
+router.get('/',userController.findAllUsers);
+router.post('/',userController.createUser);
+router.put('/',userController.updateUser);
+router.delete('/',userController.deleteUser);
+router.get('/find/:username',userController.findUser);
+router.post('/orders',apiUserOrder.createUserOrder);
+//router.get('/users-address',usersController.findUserWAddress);
+
+router.post('/addUsers',userController.createUsersAddress,userController.findAllUsers);
+
+export default(router)
